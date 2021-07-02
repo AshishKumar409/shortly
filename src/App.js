c833b8da747cbdddc5be2e8e9b7b4c5b0cec618f
AshishKumar409 <ashishkumarnath9@gmail.com>
2021-07-02 23:21:07 +0530

    shortly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Navbar} from "./components/"
-import {BrowserRouter as Router ,Switch,Route}  from  'react-router-dom'
+import {BrowserRouter as Router}  from  'react-router-dom'
 import GlobalStyle from './globalStyles';
 import Home from './pages/HomePage/Home';
 
